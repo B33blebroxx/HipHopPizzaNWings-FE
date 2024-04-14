2fa731c2f783f76ffd5d1f179d939d1375c7be55
B33blebroxx <b33blebroxx@gmail.com>
2024-04-13 21:42:30 -0500

    Create and update order working.

--- api/orderApi.js
+++ api/orderApi.js
@@ -53,6 +53,41 @@ const deleteOrder = (id) => new Promise((resolve, reject) => {
     .catch(reject);
 });
 
+const createOrder = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/orders/new`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  }).then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const updateOrder = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/orders/update/${payload.id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  }).then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const getOrderTypes = () => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/orderType`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }).then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
 export {
-  getAllOrders, getOrderDetails, getOrderItems, deleteOrder,
+  getAllOrders, getOrderDetails, getOrderItems, deleteOrder, createOrder, updateOrder, getOrderTypes,
 };
---
+++ pages/order/edit/[id].js
@@ -0,0 +1,16 @@
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { getOrderDetails } from '../../../api/orderApi';
+import OrderForm from '../../../components/Forms/OrderForm';
+
+export default function EditOrder() {
+  const [editOrder, setEditOrder] = useState({});
+  const router = useRouter();
+  const { id } = router.query;
+
+  useEffect(() => {
+    getOrderDetails(id).then(setEditOrder);
+  }, [id]);
+
+  return (<OrderForm orderObj={editOrder} />);
+}
